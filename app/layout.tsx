e285d914dd17b30fca34bf2c07b9bff509a6d3e4
Danio <daniofioredev@gmail.com>
2025-07-09 17:08:51 +0200

    Rename project from "nim" to "inkeisoft"

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,15 +13,15 @@ export const viewport: Viewport = {
 }
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://nim-fawn.vercel.app/'),
+  metadataBase: new URL('https://www.inkeisoft.com/'),
   alternates: {
     canonical: '/'
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Inkeisoft',
+    template: '%s | Inkeisoft'
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
+  description:  'Inkeisoft is an idea of minimal Software Development',
 };
 
 const geist = Geist({
