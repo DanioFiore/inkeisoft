7ffb479b9727728723a84821508b8bf0b26c3b2c
Danio <daniofioredev@gmail.com>
2025-07-12 19:07:45 +0200

    update: revise work experience entries for accuracy and consistency

--- app/data.ts
+++ app/data.ts
@@ -49,29 +49,21 @@ export const PROJECTS: Project[] = [
 
 export const WORK_EXPERIENCE: WorkExperience[] = [
   {
-    company: 'Reglazed Studio',
-    title: 'CEO',
-    start: '2024',
+    company: 'SysCo Srl',
+    title: 'Software Developer',
+    start: '2022',
     end: 'Present',
-    link: 'https://ibelick.com',
+    link: 'https://sysco.biz/en',
     id: 'work1',
   },
   {
     company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
-    id: 'work2',
-  },
-  {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
+    title: 'Software Developer',
+    start: '2025',
     end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work3',
-  },
+    link: 'https://inkeisoft.com',
+    id: 'work2',
+  }
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
